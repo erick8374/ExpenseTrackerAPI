8ea73eb9c5292fedf7113e7fd1ab58b0e686108e
Érick L. de Paula <erick.ldepaula@gmail.com>
2024-11-07 01:03:07 -0300

    próximos passos

--- src/routes/expenseRoute.ts
+++ src/routes/expenseRoute.ts
@@ -5,10 +5,11 @@ const expenseController = new ExpenseController();
 const router = express.Router()
 
 router.post('/expense', expenseController.create)
-router.get('/expenses', expenseController.getAll)
+router.get('/expenses', expenseController.getAll) //adcionar um parametro query que pode filtrar por usuário ou/e categoria
 router.get('/expense/:id', expenseController.getById)
 router.get('/expense/user/:userId', expenseController.getByUser)
-router.put('/expense/:id', expenseController.update) //não está funcionando
+// router.get('/expense/category/:categoryId', expenseController.getByCategory)
+router.put('/expense/:id', expenseController.update)
 router.delete('/expense/:id', expenseController.delete)
 
 export default router
--- src/routes/incomeRoute.ts
+++ src/routes/incomeRoute.ts
@@ -5,9 +5,10 @@ const incomeController = new IncomeController();
 const router = express.Router()
 
 router.post('/income', incomeController.create)
-router.get('/incomes', incomeController.getAll)
+router.get('/incomes', incomeController.getAll) //adcionar um parametro query que pode filtrar por usuário ou/e account
 router.get('/income/:id', incomeController.getById)
 router.get('/income/user/:userId', incomeController.getByUser)
+// router.get('/income/account/:accountID', incomeController.getByAccount)
 router.put('/income/:id', incomeController.update) //não está funcionando
 router.delete('/income/:id', incomeController.delete)
 
